981cd34b3794cfa46c992b1b0331542ca4be38e0
Juan Siro <juan.manuel.siro@shiphero.com>
2025-03-02 10:09:13 -0300

    Add ShipmentTypes model

--- project/src/models/models.py
+++ project/src/models/models.py
@@ -21,18 +21,25 @@ class Shipments(MyBase):
     __tablename__ = 'shipments'
 
     id = Column(Integer, primary_key=True)
-    account_id = Column(Integer)
+    account_id = Column(Integer, ForeignKey('accounts.id'))
     created_at = Column(DateTime)
     country = Column(VARCHAR(45))
     is_billed = Column(Integer)
+
+class ShipmentTypes(MyBase):
+    
+    __tablename__ = 'shipment_types'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(VARCHAR(45))
 
 class Rates(MyBase):
 
     __tablename__ = 'rates'
 
     id = Column(Integer, primary_key=True)
-    account_id = Column(Integer)
-    shipment_type = Column(Enum("NATIONAL", "INTERNATIONAL"))
+    account_id = Column(Integer, ForeignKey('accounts.id'))
+    shipment_type_id = Column(Integer, ForeignKey('shipment_types.id'))
     price = Column(DECIMAL(4,2))
     created_at = Column(DateTime)
     updated_at = Column(DateTime)
@@ -42,7 +49,7 @@ class Invoices(MyBase):
     __tablename__ = 'invoices'
 
     id = Column(Integer, primary_key=True)
-    account_id = Column(Integer)
+    account_id = Column(Integer, ForeignKey('accounts.id'))
     issued_at = Column(DateTime)
     invoice_number = Column(VARCHAR(128))
     status = Column(Enum("UNPAID", "PAID", "VOIDED"))
@@ -56,6 +63,7 @@ class InvoiceItems(MyBase):
 
     id = Column(Integer, primary_key=True)
     invoice_id = Column(Integer, ForeignKey('invoices.id'))
+    shipment_type_id = Column(Integer, ForeignKey('shipment_types.id'))
     description = Column(VARCHAR(128))
     quantity = Column(Integer)
     unit_price = Column(DECIMAL(9,2))
